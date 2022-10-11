841786cf04fc2d9c408b0ae008e38f02fea3dda4
beatricezana <beatricezana44@gmail.com>
2022-10-11 08:00:41 -0700

    classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines a Class Square"""
+
+class square:
+    """An empty square class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class square:
+    def __init__(self,size):
+        Args:
+            size (int): size of a side of the square
+            self.__size = size
