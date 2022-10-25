36cc6612d34b8e1d73d55ac60bebb22021ff6629
beatricezana <beatricezana44@gmail.com>
2022-10-25 06:30:03 -0700

    inputoutput

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@ Contains the read_file function
 
 
 def read_file(filename=""):
-     """""reads a text file(UTF8) and prints it to stdout"""
-    with open(filename, "r", encoding="utf_8") as f:
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
